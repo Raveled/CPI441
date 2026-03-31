2a33a0d59e4f2288991c366351c949a90da8c56f
TylerFujikawa <tylerfujikawa@gmail.com>
2026-03-31 15:12:32 -0700

    Boss Start

--- Assets/Scripts/Entity/Non Player Entity/Boss.cs
+++ Assets/Scripts/Entity/Non Player Entity/Boss.cs
@@ -1,72 +1,20 @@
 using UnityEngine;
-using System.Collections;
-using PurrNet;
-using System.Collections.Generic;
 
-public class Core : NonPlayerEntity 
+public class Boss : NonPlayerEntity
 {
-    [Header("Core Setup")]
-    [SerializeField] Minion minionPrefab = null;
-    [SerializeField] SO_EntityStatBlock minionStatblock = null;
+    [Header("Boss Setup")]
     [SerializeField] float rotateAngularSpeed = 10f;
-    int numMinionsInWave = 5;
-    [SerializeField] float timeBetweenMinionsInWave = 1f;
-    [SerializeField] Transform[] waveSpawnOrigins = null;
-    [SerializeField] List<Transform> waypoints_Path1 = new List<Transform>();
-    [SerializeField] List<Transform> waypoints_Path2 = new List<Transform>();
-    [SerializeField] Core enemyCore = null;
-    bool canSpawnMinions = true;
 
     Vector3 basePOSThis;
     Vector3 basePOSTarget;
 
-    GameManager gameManager;
-    protected override void Start() {
-        gameManager = FindFirstObjectByType<GameManager>();
-        base.Start();
-    }
     protected override void Die(Entity damageOrigin) {
         base.Die(damageOrigin);
 
         if (!isServer) return; // Only execute end game logic on the server
 
-        gameManager.GameEnd(team);
-    }
-    //Start the spawning of a minion wave
-    public void SpawnWave() {
-        if (!canSpawnMinions) return;
-        if (!isServer) return; // Only server spawns minions
-        
-        if (GetIsDead()) return;
-
-        StartCoroutine(SpawnMinion());
-    }
-
-    //Spawn the minion in waves
-    IEnumerator SpawnMinion() {
-        if (!isServer) yield break; // Only server spawns minions
-
-        //Spawn a minion every timeBetweenMinionsInWave seconds according to numMinionsInWave
-        for(int i = 0; i < numMinionsInWave; i++) {
-            if (!canSpawnMinions) yield break;
-            if (isDead) yield break;
-
-            for(int j = 0; j < waveSpawnOrigins.Length; j++) {
-                Minion m = Instantiate(minionPrefab, waveSpawnOrigins[j].position, waveSpawnOrigins[j].rotation, waveSpawnOrigins[j]);
-                m.SetTeam(team);
-                m.SetStatblock(minionStatblock);
-                m.SetEnemyCore(enemyCore);
-
-                //Path 1 is the one spawning from waveSpawnOrigins[0]
-                var x = j == 0 ? waypoints_Path1 : waypoints_Path2;
-                m.SetWaypoints(x);
-            }
-            yield return new WaitForSeconds(timeBetweenMinionsInWave);
-        }
-    }
-    public override void Freeze(bool freezeNPE) {
-        base.Freeze(freezeNPE);
-        canSpawnMinions = !freezeNPE;
+        //REMOVE WHEN ANIMATION IS IN
+        Destroy(gameObject);
     }
     protected override void ServerUpdate() {
         Entity currentTarget = GetTarget();
@@ -74,8 +22,7 @@ public class Core : NonPlayerEntity
             ResetTarget();
         }
 
-        if (enemyCore) {
-            if (isDead) return;
+        if (!isDead) {
             FindTarget();
             currentTarget = GetTarget();
             Rotate(currentTarget);
@@ -129,12 +76,5 @@ public class Core : NonPlayerEntity
             Quaternion targetRotation = Quaternion.LookRotation(direction);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateAngularSpeed / 2 * Time.deltaTime);
         }
-    }
-    //Setter
-    public void SetMinionStatblock(SO_EntityStatBlock stats) {
-        minionStatblock = stats;
-    }
-    public void SetNumMinionsInWave(int num) {
-        numMinionsInWave = num;
     }
 }
