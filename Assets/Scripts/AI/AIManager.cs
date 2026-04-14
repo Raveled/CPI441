d49d50e879a4f22c3ec832094c80a2566e4df2e5
Tyler Fujikawa <tylerfujikawa@gmail.com>
2026-04-14 15:18:53 -0700

    Ending game logic

--- Assets/Scripts/AI/AIManager.cs
+++ Assets/Scripts/AI/AIManager.cs
@@ -58,6 +58,7 @@ public class AIManager : MonoBehaviour
         Debug.Log(response);
         entityData.LoadFromJSONString(response);
         entityData.SaveToJSON();
+        FindFirstObjectByType<GameManager>().SendToLobby();
     }
 
 
