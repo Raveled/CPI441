7b25c3a12ce41e8d3603bec39784928ea8a5999c
Jacob <jacstephen@cox.net>
2026-04-27 15:15:31 -0700

    PP Change

--- Assets/Scripts/Entity/Player.cs
+++ Assets/Scripts/Entity/Player.cs
@@ -50,7 +50,7 @@ public class Player : Entity
 
     private IEnumerator DelayedSpawn(bool asServer)
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.05f);
 
         base.OnSpawned(asServer);
 
